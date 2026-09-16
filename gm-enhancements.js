afdc4f90b697d290076c7eb167d679cdd77f9b2e
ChrisWootton <58333882+ChrisWootton@users.noreply.github.com>
2026-09-16 22:47:12 +0100

    Add mobile-first GM experience

--- gm-enhancements.js
+++ gm-enhancements.js
@@ -1,4 +1,4 @@
-/* Breezus NFL GM Enhancement Loader v2.5.13 */
+/* Breezus NFL GM Enhancement Loader v2.6.0 */
 (function(){
   'use strict';
   function gmDataReady(){
@@ -33,6 +33,7 @@
       .then(function(){return load('gm-v25-live-availability.js');})
       .then(function(){return load('gm-v25-trending.js');})
       .then(function(){return load('gm-v25-def-compare.js');})
+      .then(function(){return load('gm-mobile-ui.js');})
       .then(function(){window.__gmEnhancementsBooting=false;})
       .catch(function(e){window.__gmEnhancementsBooting=false;console.error('GM enhancement loader failed',e);});
   });
