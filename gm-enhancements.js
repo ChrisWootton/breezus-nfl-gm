79fe122afd8188b67e3753956004a994ddebbc36
ChrisWootton <58333882+ChrisWootton@users.noreply.github.com>
2026-09-16 23:00:10 +0100

    Load actionable start sit board

--- gm-enhancements.js
+++ gm-enhancements.js
@@ -1,4 +1,4 @@
-/* Breezus NFL GM Enhancement Loader v2.7 */
+/* Breezus NFL GM Enhancement Loader v2.7.1 */
 (function(){
   'use strict';
   function gmDataReady(){try{return typeof S!=='undefined'&&S&&Array.isArray(S.rosters)&&S.rosters.length>0&&S.players&&Object.keys(S.players).length>0&&S.me}catch(e){return false}}
@@ -16,6 +16,7 @@
       .then(function(){return load('gm-mobile-ui.js')})
       .then(function(){return load('gm-v26-ux.js')})
       .then(function(){return load('gm-v27-weekly-gm.js')})
+      .then(function(){return load('gm-v27-action-board.js')})
       .then(function(){window.__gmEnhancementsBooting=false})
       .catch(function(e){window.__gmEnhancementsBooting=false;console.error('GM enhancement loader failed',e)})
   })
